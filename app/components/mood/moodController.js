6b53864a3144f0e57335104f590df3f2ed427fc2
Andrew Bythell <abythell@ziptrek.com>
2015-10-01 10:12:53 -0700

    use watch instead of rootScope for date updates

--- app/components/mood/moodController.js
+++ app/components/mood/moodController.js
@@ -1,59 +1,52 @@
 (function () {
 
     angular.module('moodModule').controller('MoodController', ['moodService',
-        '$rootScope', 'dateService', '$scope', 'userService', function (moodService,
-                $rootScope, dateService, $scope, userService) {
+        'dateService', '$scope', 'userService', function (moodService,
+                dateService, $scope, userService) {
 
-            this.moodList = moodService.moodList;            
+            this.moodList = moodService.moodList;
 
+            /**
+             * Asyncronously load the mood data for the selected date.             
+             */
             var updateMood = function () {
                 var moodAM = moodService.getMoods(dateService.selectedDate, 'am');
                 moodAM.$loaded().then(function (data) {
                     $scope.moodAM = data;
-                }).catch(function(error) {
-                    console.log(error);                
                 });
-                
+
                 var moodPM = moodService.getMoods(dateService.selectedDate, 'pm');
                 moodPM.$loaded().then(function (data) {
                     $scope.moodPM = data;
-                }).catch(function(error) {
-                    console.log(error);                
                 });
             };
-            
+
             /**
              * Moods can only be edited for "today", unless the user is an 
              * administrator in which case they can edit any day.
              * @returns {undefined}
              */
             var setEditable = function () {
-                var currentUser = userService.getCurrentUser();
-                currentUser.$loaded().then(function(data) {
-                    return data.admin;                    
-                }).then(function(admin) {
+                userService.isCurrentUserAdmin().then(function (admin) {
                     if (admin | dateService.isToday()) {
                         $scope.canEdit = true;
                     } else {
                         $scope.canEdit = false;
                     }
                 });
-            }
-            
-            /*
-             * Set initial state.
-             */
-            updateMood();
-            setEditable();
+            };
 
             /*
-             * Update the data when the date changes.
+             * Fetch new data when the date changes - this includes the 
+             * initial load.
              */
-            $rootScope.$on('dateChangeEvent', function (event, date) {
-               updateMood(); 
-               setEditable();
-            });            
-            
+            $scope.$watch(function () {
+                return dateService.selectedDate;
+            }, function () {
+                updateMood();
+                setEditable();
+            }, true);
+
         }]);
 
 })();
