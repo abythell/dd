f48bc58b498abf25d177fb5c048ed09d37a517f0
abythell <abythell@ieee.org>
2015-10-02 08:45:29 -0700

    qty control for select items only

--- app/components/supplies/suppliesController.js
+++ app/components/supplies/suppliesController.js
@@ -4,34 +4,8 @@
         'dateService', '$scope', 'userService', function (suppliesService,
                 dateService, $scope, userService) {
                         
-            $scope.items = suppliesService.getSupplies(dateService.selectedDate);
-
-            /*
-             * When a button is clicked, increment the quantity and the label,
-             * then save the updates.              
-             */
-            $scope.buttonChanged = function(id) {
-                var item = $scope.items.$getRecord(id);
-                                
-                item.qty = item.qty + 1;
-                if (item.qty > item.max) item.qty = 0;
-                
-                /*
-                 * Update the label to show the qty.
-                 */
-                if (item.qty) {
-                    item.label = item.name + ' (' + item.qty + ')';
-                    item.selected = true;
-                } else {
-                    item.label = item.name;
-                    item.selected = false;
-                }
-                
-                /*
-                 * Save the changes
-                 */
-                $scope.items.$save(item);                                
-            };
+            $scope.singleItems = suppliesService.singleItemsList;
+            $scope.multiItems = suppliesService.multiItemsList;
             
             /*
              * Determine if the supplies for this day can be changed or not.             
@@ -46,6 +20,13 @@
                 });
             };
             
+            var updateSupplies = function() {
+                var obj = suppliesService.getSupplies(dateService.selectedDate);
+                obj.$loaded().then(function() {
+                    $scope.supplies = obj;
+                });
+            };
+            
             /*
              * Fetch new data when the date changes - this includes the 
              * initial load.
@@ -53,7 +34,7 @@
             $scope.$watch(function () {
                 return dateService.selectedDate;
             }, function () {
-                $scope.items = suppliesService.getSupplies(dateService.selectedDate);
+                updateSupplies();
                 setEditable();
             }, true);
 
--- app/components/supplies/suppliesService.js
+++ app/components/supplies/suppliesService.js
@@ -1,54 +1,38 @@
 (function () {
-    angular.module('suppliesModule').service('suppliesService', ['$firebaseArray',
-        '$filter', function ($firebaseArray, $filter) {
+    angular.module('suppliesModule').service('suppliesService', ['$firebaseObject',
+        '$filter', function ($firebaseObject, $filter) {
 
             /*
              * URL of firebase app
              */
             var firebaseUrl = 'https://brilliant-inferno-6689.firebaseio.com';
             var ref = new Firebase(firebaseUrl);
-            var suppliesService = {};
 
             /*
              * List all 'supplies' here.  You can add new ones as needed, but
              * removing or renaming supplies will affect historical data.
              */
-            var supplies = [
-                {name: 'Diapers', max: 5},
-                {name: 'Medicine Syringe 10cc', max: 5},
-                {name: 'Flushing Syringe 20cc', max: 5},
-                {name: 'Venting Syringe 60cc', max: 5},
-                {name: 'Feeding Tube (thin, bent)', max: 5},
-                {name: 'Venting Tube (thick, straight)', max: 5},
-                {name: 'Duocal', max: 5},
-                {name: 'Baclofen', max: 5},
-                {name: 'Pill Crusher', max: 5},
-                {name: 'Bottle (mix meds)', max: 5},
-                {name: 'Food', max: 5},
-                {name: 'Syringe Tips', max: 5}
+            this.singleItemsList = [
+                'Diapers',
+                'Medicine Syringe 10cc',
+                'Flushing Syringe 20cc',
+                'Venting Syringe 60cc',
+                'Feeding Tube (thin, bent)',
+                'Venting Tube (thick, straight)',
+                'Duocal',
+                'Baclofen',
+                'Pill Crusher',
+                'Bottle (mix meds)'
+            ];
+            
+            this.multiItemsList  = [
+                'Food',
+                'Syringe Tips'
             ];
                         
-            var getDefaultItemList = function () {                
-                for (var i = 0; i < supplies.length; i++) {
-                    supplies[i].qty = 0;                    
-                    supplies[i].selected = false;
-                    supplies[i].label = supplies[i].name;
-                };
-                return supplies;
-            };
-
             this.getSupplies = function (date) {
                 var key = $filter('date')(date, "yyyy-MM-dd");
-                var obj = $firebaseArray(ref.child('supplies').child(key));
-                obj.$loaded().then(function(data) {
-                    if (data.length === 0) {
-                        var items = getDefaultItemList();
-                        for (var i=0; i< items.length; i++) {
-                            obj.$add(items[i]);
-                        }                        
-                    }
-                });
-                return obj;
+                return $firebaseObject(ref.child('supplies').child(key));
             };
 
         }]);
