265719a148c544effbfdcc7f6b4159609a9ac22b
Andrew Bythell <abythell@ziptrek.com>
2015-10-05 16:00:53 -0700

    make alert editable by admins

--- app/components/alert/alertController.js
+++ app/components/alert/alertController.js
@@ -3,15 +3,60 @@
     angular.module('alertModule').controller('AlertController', ['alertService',
         'dateService', '$scope', 'userService', function (alertService,
                 dateService, $scope, userService) {
-            
-            $scope.templateUrl = "editPopover.html";
-            
+
+            /*
+             * This message will be displayed if a custom message has not
+             * been set.  The default message is displayed in a "warning"
+             * style alert.                 
+             */
+            var defaultMessage = "This tool is a means to communicate important "
+                    + "information about Declan and the events of his day.  Please "
+                    + "remember that we need your input to talk to Declan about what has "
+                    + "transpired, because he cannot tell us himself.";
+
+            /*
+             * When hidden, the alert message is not shown.
+             */
             this.hidden = false;
-            
-            this.click = function() {
-              this.hidden = true;  
+
+            /*
+             * In edit mode, a text area is displayed instead of the alert.
+             */
+            this.editMode = false;
+
+            /*
+             * Switch from alert to edit mode.
+             */
+            this.edit = function () {
+                if ($scope.canEdit) {
+                    this.editMode = true;
+                }
             };
-            
+
+            /*
+             * Save text and switch back to alert mode.  Edited alerts
+             * are displayed in a "danger" style alert.
+             */
+            this.save = function () {
+                this.editMode = false;
+                if ($scope.alert.$value.length > 0) {
+                    $scope.alert.$save();
+                    $scope.alertClass = "alert-danger";
+                }
+            };
+
+            /*
+             * Remove the custom message and revert back to the default.             
+             */
+            this.reset = function () {
+                this.editMode = false;
+                $scope.alert.$remove().then(function () {
+                    $scope.alert.$value = defaultMessage;
+                });
+
+                $scope.alertClass = "alert-warning";
+            };
+
             /**
              * Asyncronously load the alert data for the selected date.             
              */
@@ -19,6 +64,12 @@
                 var alert = alertService.getAlert(dateService.selectedDate);
                 alert.$loaded().then(function (data) {
                     $scope.alert = data;
+                    if (data.$value === null) {
+                        data.$value = defaultMessage;
+                        $scope.alertClass = 'alert-warning';
+                    } else {
+                        $scope.alertClass = 'alert-danger';
+                    }
                 });
             };
 
@@ -43,7 +94,7 @@
                 return dateService.selectedDate;
             }, function () {
                 updateAlert();
-                setEditable();            
+                setEditable();
             }, true);
 
         }]);
