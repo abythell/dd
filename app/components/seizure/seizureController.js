bcdaf87f3fa86066516dbe5a9ce913678355ed2d
abythell <abythell@ieee.org>
2015-10-06 22:34:46 -0700

    finish seizure implementation

--- app/components/seizure/seizureController.js
+++ app/components/seizure/seizureController.js
@@ -1,23 +1,28 @@
 (function () {
 
     angular.module('seizureModule').controller('SeizureController', ['dateService',
-        '$scope', 'userService', 'seizureService','$filter', function (dateService, $scope,
+        '$scope', 'userService', 'seizureService', '$filter', function (dateService, $scope,
                 userService, seizureService, $filter) {
 
-            $scope.start = new Date();
-            $scope.duration = 0;
-            
-            this.getDuration = function() {
-                var min = Math.round($scope.duration / 60);
-                var seconds = $scope.duration % 60;
+            /*
+             * Convert seconds into mm:ss.  There are likely directives
+             * and modules that can do this for us, but this is simple
+             * enough not to warrant additional dependencies.
+             */
+            this.getDuration = function (ticks) {
+                var min = Math.round(ticks / 60);
+                var seconds = ticks % 60;
                 var date = new Date(0, 0, 0, 0, min, seconds);
                 return $filter('date')(date, 'mm:ss');
             };
-            
-            /*
-             * Determine if the seizure for this day can be changed or not.             
-             */
 
+            /*
+             * Determine if the seizure for this day can be changed or not. 
+             * Admins can add activity any day, but other users can only add
+             * activity for 'today'.  Also, as enforced in the view, admins
+             * can delete any observations they have recorded.  Other users
+             * can only delete observations made by them "today".        
+             */
             var setEditable = function () {
                 userService.isCurrentUserAdmin().then(function (admin) {
                     if (admin | dateService.isToday()) {
@@ -28,27 +33,40 @@
                 });
             };
 
+            /*
+             * Get a list of activity observations for the selected date.  Also
+             * resets the form.  Called on initial load and whenever the date
+             * changes.
+             */
             var getActivity = function () {
+                $scope.start = new Date();
+                $scope.duration = 0;
                 userService.getCurrentUser().$loaded().then(function (user) {
                     $scope.uuid = user.$id;
-                }).then(function () {
                     $scope.activity = seizureService.getSeizure(dateService.selectedDate);
                 });
             };
 
+            /*
+             * Add a new observation.
+             */
             this.addActivity = function () {
                 userService.getCurrentUser().$loaded().then(function (user) {
                     var newActivity = {
                         who: user.name,
-                        start: $scope.start,
+                        start: $scope.start.getTime(),
                         duration: $scope.duration,
                         notes: $scope.notes,
                         uuid: user.$id
                     };
                     $scope.activity.$add(newActivity);
+                    
+                    /*
+                     * Reset the form.
+                     */
                     $scope.what = '';
-                    $scope.start = '';
-                    $scope.duration = '';
+                    $scope.start = new Date();
+                    $scope.duration = 0;
                     $scope.notes = '';
                 });
             };
