2355e016bf866ffb0151f7a58f96343079090d0f
Andrew Bythell <abythell@ziptrek.com>
2015-10-07 15:59:03 -0700

    set default qty to 0 if undefined

--- app/components/supplies/suppliesController.js
+++ app/components/supplies/suppliesController.js
@@ -3,10 +3,10 @@
     angular.module('suppliesModule').controller('SuppliesController', ['suppliesService',
         'dateService', '$scope', 'userService', function (suppliesService,
                 dateService, $scope, userService) {
-                        
+
             $scope.singleItems = suppliesService.singleItemsList;
             $scope.multiItems = suppliesService.multiItemsList;
-            
+
             /*
              * Determine if the supplies for this day can be changed or not.             
              */
@@ -19,14 +19,18 @@
                     }
                 });
             };
-            
-            var updateSupplies = function() {
+
+            var updateSupplies = function () {
                 var obj = suppliesService.getSupplies(dateService.selectedDate);
-                obj.$loaded().then(function() {
+                obj.$loaded().then(function () {
                     $scope.supplies = obj;
+                    for (var i=0; i< $scope.multiItems.length; i++) {
+                        $scope.supplies[$scope.multiItems[i]] = 
+                                $scope.supplies[$scope.multiItems[i]] || 0;
+                    }
                 });
             };
-            
+
             /*
              * Fetch new data when the date changes - this includes the 
              * initial load.
