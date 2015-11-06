4117d5e4961ba68f0952ee6b5ddc414dc9b6e6ab
abythell <abythell@ieee.org>
2015-11-05 22:31:23 -0800

    use minified script produced by gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,29 @@
 var gulp = require('gulp'),
-    autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
     jshint = require('gulp-jshint'),
     uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
     rename = require('gulp-rename'),
     concat = require('gulp-concat'),
-    notify = require('gulp-notify'),
-    cache = require('gulp-cache'),
-    livereload = require('gulp-livereload'),
-    del = require('del');
+    watch = require('gulp-watch');
 
-gulp.task('clean', function () {  
-  return gulp.src('dist', {read: false})
-    .pipe(clean());
+gulp.task('lint', function() {
+    return gulp.src('./app/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
 });
 
-gulp.task('test', function() {
-   return gulp.src('./app/**/*.js')
-           .pipe(jshint()).pipe(jshint.reporter('default'))
-	   .pipe(concat('dd.js'))
-	   .pipe(gulp.dest(''))
-	   .pipe(uglify())
-	   .pipe(rename('dd.min.js'))
-	   .pipe(gulp.dest(''))
+gulp.task('scripts', function() {
+    return gulp.src('./app/**/*.js')
+        .pipe(concat('dd.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('dd.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
 });
+
+gulp.task('watch', function() {
+    gulp.watch('./app/**/*.js', ['lint', 'scripts']);
+});
+
+// Default Task
+gulp.task('default', ['lint', 'scripts']);
